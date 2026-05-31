4fff786850fa6445eef2a11af7e019d4b8a18848
bowo-prasetyo <bpraz@yahoo.com>
2026-05-31 12:58:18 +0900

    Update router.js

--- router.js
+++ router.js
@@ -1,7 +1,8 @@
 const menu = `<div class="card">
         <button @click="$router.push('/')">Simulator</button>
         <button @click="$router.push('/manual')">User Manual</button>
-        <button @click="this.$globalThis.open('https://bowo-prasetyo.github.io/quantum-1qubit/', '_blank')">1 Qubit</button>
+        <button @click="$globalThis.open('https://bowo-prasetyo.github.io/quantum-1qubit/', '_blank')">1 Qubit</button>
+        <button @click="$globalThis.open('https://bowo-prasetyo.github.io/quantum-3qubits/', '_blank')">3 Qubits</button>
       </div>`;
 
 const Home = {
